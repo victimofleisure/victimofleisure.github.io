c8cc3f2eaa3d6fcb1b83b3c2c0253be95696ba5c
Chris Korda <victimofleisure@users.noreply.github.com>
2025-11-26 13:42:34 +0100

    update events, press, news, etc

--- gen.bat
+++ gen.bat
@@ -1,8 +1,12 @@
 echo updates *ALL* pages with new navigation, are you SURE? Ctrl+C to abort
 pause
+attrib +r granularism.html
 attrib +r atonal\*.*
 attrib +r img\*.*
 attrib +r math\*.*
 attrib +r tlor\*.*
+attrib +r misc\pitch_class_histograms.html
+attrib +r misc\PlainBobForSevenBells.html
 C:\Chris\Tools\navgen template.html .
+
 
